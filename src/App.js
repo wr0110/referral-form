ace3d36e05d81c1a5cd95ea2411ad4d4d0bc2a45
witty <wittyraccoon0110@gmail.com>
2022-10-17 23:35:54 +0300

    some role changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ReferralForm from "./pages/referral-form/referral-form.component";
 import SuccessForm from "./pages/success-form/success-form.component";
